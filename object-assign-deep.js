38fedf73dd59326f8427b92abd3c03af8459f4e7
Yusuke Kamiyamane <y@y14e.com>
2025-04-11 21:39:10 +0900

    20250411

--- object-assign-deep.js
+++ object-assign-deep.js
@@ -1,8 +1,8 @@
 function objectAssignDeep(target, ...sources) {
-  let isObject = value => value !== null && typeof value === 'object' && !Array.isArray(value);
+  const isObject = value => value !== null && typeof value === 'object' && !Array.isArray(value);
   sources.forEach(source => {
     Object.entries(source || {}).forEach(([key, value]) => {
-      let targetValue = target[key];
+      const targetValue = target[key];
       target[key] = isObject(value) ? objectAssignDeep(isObject(targetValue) ? structuredClone(targetValue) : {}, value) : structuredClone(value);
     });
   });
--- object-assign-deep.ts
+++ object-assign-deep.ts
@@ -1,8 +1,8 @@
 function objectAssignDeep(target: {}, ...sources: any[]): any {
-  let isObject = (value: any): boolean => value !== null && typeof value === 'object' && !Array.isArray(value);
+  const isObject = (value: any): boolean => value !== null && typeof value === 'object' && !Array.isArray(value);
   sources.forEach(source => {
     Object.entries(source || {}).forEach(([key, value]) => {
-      let targetValue = (target as any)[key];
+      const targetValue = (target as any)[key];
       (target as any)[key] = isObject(value) ? objectAssignDeep(isObject(targetValue) ? structuredClone(targetValue) : {}, value) : structuredClone(value);
     });
   });
