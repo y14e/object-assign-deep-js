a50aae20ce79bd8b3249b00b8d688cb95a12c83a
Yusuke Kamiyamane <y@y14e.com>
2025-09-26 10:05:45 +0900

    20250926

--- object-assign-deep.js
+++ object-assign-deep.js
@@ -1,18 +1,14 @@
 export function objectAssignDeep(target, ...sources) {
-  function isPlainObject(value) {
-    return Object.prototype.toString.call(value) === '[object Object]';
-  }
-  function safeStructuredClone(value) {
+  const isPlainObject = value => Object.prototype.toString.call(value) === '[object Object]';
+  const safeStructuredClone = value => {
     try {
       return structuredClone(value);
     } catch {
       return Array.isArray(value) ? [...value] : isPlainObject(value) ? objectAssignDeep({}, value) : value;
     }
-  }
+  };
   sources.forEach(source => {
-    if (!source || typeof source !== 'object') {
-      return;
-    }
+    if (!source || typeof source !== 'object') return;
     Object.entries(source).forEach(([key, value]) => {
       const targetValue = target[key];
       target[key] = isPlainObject(value) && isPlainObject(targetValue) ? objectAssignDeep(targetValue, value) : safeStructuredClone(value);
--- object-assign-deep.ts
+++ object-assign-deep.ts
@@ -1,20 +1,16 @@
 type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
 
 export function objectAssignDeep<T extends object, U extends object[]>(target: T, ...sources: U): T & UnionToIntersection<U[number]> {
-  function isPlainObject(value: unknown): value is Record<string, any> {
-    return Object.prototype.toString.call(value) === '[object Object]';
-  }
-  function safeStructuredClone<T>(value: T): T {
+  const isPlainObject = (value: unknown): value is Record<string, any> => Object.prototype.toString.call(value) === '[object Object]';
+  const safeStructuredClone = <T>(value: T): T => {
     try {
       return structuredClone(value);
     } catch {
       return Array.isArray(value) ? ([...value] as T) : isPlainObject(value) ? (objectAssignDeep({}, value) as T) : value;
     }
-  }
+  };
   sources.forEach(source => {
-    if (!source || typeof source !== 'object') {
-      return;
-    }
+    if (!source || typeof source !== 'object') return;
     Object.entries(source).forEach(([key, value]) => {
       const targetValue = target[key as keyof T];
       target[key as keyof T] = isPlainObject(value) && isPlainObject(targetValue) ? objectAssignDeep(targetValue, value) : safeStructuredClone(value);
